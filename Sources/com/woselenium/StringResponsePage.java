428a045997f717043d5b32704c984aba923ab6d9
Denis Frolov <dfrolov@demax.ru>
2013-06-07 10:58:46 +0400

    Wait for body tag to be visible before trying to get it's text
    Looks like FireFox on Linux can sometimes try to get text before body is visible resulting in the following exception:
    
    org.openqa.selenium.remote.ErrorHandler$UnknownServerException: Unable to locate element: {"method":"tag name","selector":"body"}

--- Sources/com/woselenium/StringResponsePage.java
+++ Sources/com/woselenium/StringResponsePage.java
@@ -1,17 +1,25 @@
 package com.woselenium;
 
 
-import org.openqa.selenium.By;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class StringResponsePage extends Page {
 	
+	@FindBy(tagName = "body")
+	private WebElement body;
+
 	public StringResponsePage(WebDriver driver) {
 		super(driver);
+		
+		wait.until(visibilityOf(body));
 	}
 
 	public String stringResponse() {
-		return driver.findElement(By.tagName("body")).getText();
+		return body.getText();
 	}
 
 }
